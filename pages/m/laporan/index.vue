dfe767b4ce4f05dd4f7644b1e266bb48f372a923
Michael Chandra <michaelchandrag114@yahoo.com>
2021-09-15 19:34:09 +0700

    update tombol download mobile

--- pages/m/laporan/index.vue
+++ pages/m/laporan/index.vue
@@ -89,9 +89,24 @@
                 </ul>
               </div>
             </div>
-
-
            
+        </div>
+        <div class="flex gap-4">
+            <div class="w-full mb-2">
+              <div class="relative w-full">
+                <button v-if="isDownload"
+                  class="w-full cursor-not-allowed  rounded-fd py-4 border-2 border-gray-500 bg-gray-200 text-gray-500 focus:outline-none">
+                  <span class="animate-spin">Downloading . . .</span>
+
+                </button>
+                <button v-if="!isDownload"
+                  class="w-full rounded-fd py-4 border-2 border-green-food bg-green-200 text-green-food focus:outline-none"
+                  @click.prevent="download()">
+                  <span v-if="!isDownload">Download</span>
+                </button>
+              </div>
+            </div>
+
         </div>
           <div class="mt-2">
             <table class="table-auto w-full">
@@ -224,6 +239,7 @@ export default {
     layout: 'mobile',
     data() {
       return {
+        isDownload: false,
         data: {},
         sorted: {},
         search: '',
@@ -292,6 +308,24 @@ export default {
             this.listLoading = false
           this.total_page = r.data.data.total_page
         })
+    },
+    download() {
+      this.isDownload = true
+      this.$axios({
+        method: 'GET',
+        url: 'me/report/atp_export?date=' + this.date + '&xlsx=true',
+        responseType: 'blob',
+      }).then(r => {
+        this.isDownload = false
+        const url = window.URL.createObjectURL(new Blob([r.data], {
+          'content-type': "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        }));
+        var fileLink = document.createElement('a');
+        fileLink.href = url;
+        fileLink.setAttribute('download', 'laporan-performa-outlet.xlsx');
+        document.body.appendChild(fileLink);
+        fileLink.click();
+      })
     }
     },
   }
