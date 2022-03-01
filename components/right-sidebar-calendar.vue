84081500cb081401b7b27e6ce3e2573eab9aabe6
Michael Chandra <michaelchandrag114@yahoo.com>
2022-03-01 11:32:24 +0700

    Update right-sidebar-calendar.vue

--- components/right-sidebar-calendar.vue
+++ components/right-sidebar-calendar.vue
@@ -9,8 +9,35 @@
         <date-picker placeholder="MM/DD/YYYY" format="MM/dd/yyyy" :inline="true" :value="date" :disabledDates="disabledDates"/>
       </client-only>
 
-
     </div>
+    
+    <div class="w-full xl:w-12/12 lg:w-12/12 sm:w-12/12 p-2 cursor-pointer items-center relative text-center">
+        <div @click.prevent="itemInStockDropdown?itemInStockDropdown=false:itemInStockDropdown=true"
+          class="border flex py-3 px-4 border-gray-300 rounded-lg w-full focus:outline-none">
+          <div class="flex-auto">
+            <span v-if="itemInStock==null">Semua Status</span>
+            <span v-if="itemInStock==1">Item Aktif</span>
+            <span v-if="itemInStock==0">Item Tidak Aktif</span>
+          </div>
+          <div>
+            <svg class="float-right mt-2" width="8" height="5" viewBox="0 0 8 5" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0.710051 1.71L3.30005 4.3C3.69005 4.69 4.32005 4.69 4.71005 4.3L7.30005 1.71C7.93005 1.08 7.48005 0 6.59005 0H1.41005C0.520051 0 0.0800515 1.08 0.710051 1.71Z"
+                fill="#9E9E9E" />
+            </svg>
+          </div>
+
+        </div>
+        <div v-if="itemInStockDropdown" class="w-full shadow-sm rounded-b-fds">
+          <ul class="w-full border-gray-300">
+            <li @click.prevent="itemInStockDropdown=false,itemInStock=null" class="bg-white px-4 py-3 w-full rounded-b-lg border-2">Semua Status</li>
+            <li @click.prevent="itemInStockDropdown=false,itemInStock=1" class="bg-white px-4 py-3 w-full border-2">Item Aktif</li>
+            <li @click.prevent="itemInStockDropdown=false,itemInStock=0" class="bg-white px-4 py-3 w-full border-2">Item Tidak Aktif</li>
+          </ul>
+        </div>
+      </div>
+
     <div class="mt-4">
       <button v-if="isDownload"
         class="w-full cursor-not-allowed  rounded-fd p-3 border-2 border-gray-500 bg-gray-200 text-gray-500 focus:outline-none">
@@ -34,6 +61,8 @@ export default {
       date: new Date(),
       formatDate: '',
       isDownload: false,
+      itemInStock: null,
+      itemInStockDropdown: false,
       disabledDates: {
         to: new Date(Date.now() - 8640000),
         from: new Date()
@@ -53,7 +82,7 @@ export default {
       this.isDownload = true
       this.$axios({
         method: 'GET',
-        url: 'me/channel_report?channel=GoFood&issued_at=' + this.formatDate + '&xlsx=true',
+        url: 'me/channel_report?channel=GoFood&issued_at=' + this.formatDate + '&xlsx=true' + '&item_in_stock=' + this.itemInStock,
         responseType: 'blob',
       }).then(r => {
         this.isDownload = false
