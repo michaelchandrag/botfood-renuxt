df3ec80beb5bac9d4d4cfe16ab1cbd516cc40e5d
aripadrian <aripadrian01@gmail.com>
2022-07-24 13:23:09 +0700

    fixing nav

--- pages/account/new.vue
+++ pages/account/new.vue
@@ -3,7 +3,11 @@
     <left-sidebar class="px-6 pt-8" />
 
     <div class="bg-gray-200 wrapper-content">
-      <div>
+      <div
+        @click.prevent="$router.push('/account')"
+        class="flex items-center gap-x-2 cursor-pointer"
+      >
+        <i class="fas fa-chevron-left"></i>
         <span class="text-title">Buat Akun Baru</span>
       </div>
       <div style="height: 24px"></div>
