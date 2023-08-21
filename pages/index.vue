a3589c508fcbd8d0af1879b74ad5b68a5ef6a71a
Michael Chandra <canzinzzzide@yahoo.co.id>
2023-08-21 21:32:55 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,15 @@
       <client-only
         ><header-navbar class="hidden md:block"></header-navbar
       ></client-only>
+      <div v-if="showInfo" class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md mb-3" role="alert">
+        <div class="flex">
+          <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+          <div>
+            <p class="font-bold">Fitur Baru!</p>
+            <p class="text-sm">Yuk join grup Whatsapp Botfood untuk mendapatkan notifikasi segera jika outlet tutup saat jam operasional! <a :href="wablasGroupUrl" class="text-blue-500" target="_blank">{{wablasGroupUrl}}</a></p>
+          </div>
+        </div>
+      </div>
       <div class="flex justify-between items-center">
         <span class="text-title">Laporan Terkini</span>
         <div class="mr-6">
@@ -229,6 +238,8 @@ export default {
       idle_items: {
         items: "",
       },
+      showInfo: false,
+      wablasGroupUrl: null,
       isLoading: true,
       listLoading: false,
       page: 1,
@@ -263,6 +274,11 @@ export default {
         const res = await this.$axios.get("/me");
         if (res.data.success) {
           this.$store.commit("user/setUser", res.data.data);
+          var data = res.data.data
+          if (data.wablas_group_url !== undefined && data.wablas_group_url != null) {
+            this.wablasGroupUrl = data.wablas_group_url
+            this.showInfo = true
+          }
         }
       } catch (error) {}
     },
