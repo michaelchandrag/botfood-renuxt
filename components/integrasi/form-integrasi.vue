2eb11b8728fd775528110389363f833fbe744430
aripadrian <aripadrian01@gmail.com>
2023-08-30 07:33:19 +0700

    add: form otp & connect api

--- components/integrasi/form-integrasi.vue
+++ components/integrasi/form-integrasi.vue
@@ -3,7 +3,7 @@
     <client-only>
       <vmodal name="form" :adaptive="true" height="auto">
         <div class="p-4 w-full">
-          <form class="w-full" autocomplete="off">
+          <form class="w-full" autocomplete="off" @submit.prevent="connectAcc">
             <h1 class="font-bold text-lg mb-4 text-center">Integrasi Channel</h1>
 
 <!--            Info requirement untuk connect-->
@@ -18,11 +18,11 @@
 <!--            Info requirement untuk connect-->
             <div class="mb-2">
               <label class="text-sm text-gray-600">Username</label>
-              <input class="p-2 w-full rounded-md border outline-none block" type="text" placeholder="Masukkan Username" v-model.trim="form.username">
+              <input :disabled="loading" class="p-2 w-full rounded-md border outline-none block" type="text" placeholder="Masukkan Username" v-model.trim="form.username">
             </div>
             <div class="mb-2">
               <label class="text-sm text-gray-600">Password</label>
-              <input class="p-2 w-full rounded-md border outline-none block" type="password" placeholder="Masukkan Password" v-model.trim="form.username">
+              <input :disabled="loading"  class="p-2 w-full rounded-md border outline-none block" type="password" placeholder="Masukkan Password" v-model.trim="form.password">
             </div>
 
             <!--            Info requirement untuk connect-->
@@ -46,18 +46,41 @@
             </div>
             <!--            Info requirement untuk connect-->
             <div class="flex justify-end gap-3 mt-3">
-              <button type="button" class="flex text-gray-500 px-4 py-2 items-center gap-1" @click.prevent="$modal.hide('form')">
+              <button type="button" :disabled="loading" class="flex text-gray-500 px-4 py-2 items-center gap-1" @click.prevent="$modal.hide('form')">
                 <icon-base name="x"/>
                 Tutup
               </button>
-              <button type="submit" class="flex px-4 py-2 items-center gap-1 bg-green-600 text-green-50 rounded-lg">
+              <button :disabled="loading" :class="loading?'bg-gray-400':'bg-green-600'" type="submit" class="flex px-4 py-2 items-center gap-1 text-green-50 rounded-lg">
                 <icon-base name="save"/>
-                Simpan
+                {{loading?'Menyimpan...':'Simpan'}}
+
               </button>
             </div>
           </form>
         </div>
       </vmodal>
+
+
+<!--      formOtp-->
+      <vmodal name="formOtp" :adaptive="true" height="auto">
+        <div class="p-4">
+          <form class="w-full" autocomplete="off" @submit.prevent="validasiOtp">
+            <h1 class="font-bold text-lg mb-4 text-center">Validasi OTP</h1>
+            <input :disabled="loadingOtp" class="p-2 w-full text-center focus:border-green-600 border-b outline-none block text-2xl" type="text" placeholder="Masukkan OTP" v-model.trim="otp">
+            <div class="flex justify-end gap-3 mt-4">
+              <button type="button" :disabled="loadingOtp" class="flex text-gray-500 px-4 py-2 items-center gap-1" @click.prevent="$modal.hide('formOtp')">
+                <icon-base name="x"/>
+                Tutup
+              </button>
+              <button :disabled="loadingOtp" :class="loadingOtp?'bg-gray-400':'bg-green-600'" type="submit" class="flex px-4 py-2 items-center gap-1 text-green-50 rounded-lg">
+                <icon-base name="save"/>
+                {{loadingOtp?'Menyimpan...':'Simpan'}}
+              </button>
+            </div>
+          </form>
+        </div>
+      </vmodal>
+
     </client-only>
   </div>
 </template>
@@ -69,7 +92,89 @@ export default  {
       form: {
         username:"",
         password:""
+      },
+      loading: false,
+      otp:'',
+      resConnect: {
+        integration: {
+          id:""
+        }
+      },
+      loadingOtp:false
+    }
+  },
+
+  methods:{
+    async connectAcc() {
+      try {
+        this.loading = true
+        const res = await this.$axios.post("me/integrations/connect", {
+          "type": "shopee_partner_web",
+          "username": this.form.username,
+          "password":  this.form.password
+        });
+        this.loading = false
+        if(res.data.success) {
+          const next = res.data.data
+          this.resConnect = res.data.data
+          if(next.next_move==='otp') {
+            this.$modal.show("formOtp")
+          } else {
+            this.$modal.hide("form")
+            this.$modal.hide("formOtp")
+            this.$emit('reload')
+          }
+        }
+
+      } catch (e) {
+        this.loading = false
+        const err = e.response.data.errors
+        if(err) {
+
+          try {
+            err.forEach(msg=>{
+              this.$toast.error(`${msg.code} : ${msg.detail}`)
+            })
+          } catch (e) {
+            console.log(e)
+          }
+        }
+
       }
+
+
+    },
+    async validasiOtp() {
+      try {
+        this.loadingOtp = true
+        const res = await this.$axios.post("me/integrations/connect_otp", {
+          "integration_id": this.resConnect.integration.id||"",
+          "otp": this.otp
+        });
+        this.loadingOtp = false
+        if(res.data.success) {
+            this.$modal.show("formOtp")
+            this.$emit('reload')
+        } else {
+
+        }
+      } catch (e) {
+        this.loadingOtp = false
+        const err = e.response.data.errors
+        if(err) {
+
+          try {
+            err.forEach(msg=>{
+              this.$toast.error(`${msg.code} : ${msg.detail}`)
+            })
+          } catch (e) {
+            console.log(e)
+          }
+        }
+
+      }
+
+
     }
   }
 }
--- pages/integrasi/index.vue
+++ pages/integrasi/index.vue
@@ -12,7 +12,7 @@
         <span class="text-title">Integrasi</span>
       </div>
         <div class="bg-white p-4 rounded-fd mt-4 min-h-[400px] grid grid-cols-2 md:grid-cols-4 gap-3 cursor-pointer">
-            <div @click.prevent="$modal.show('form')" class="bg-gray-50 relative border text-gray-700 h-56 rounded-fd  flex items-center justify-center">
+            <div v-if="!loading" @click.prevent="$modal.show('form')" class="bg-gray-50 relative border text-gray-700 h-56 rounded-fd  flex items-center justify-center">
                 <div class="text-center">
                     <span class="text-[90px] block">+</span>
                 </div>
@@ -20,12 +20,19 @@
                     Tambah Integrasi
                 </div>
             </div>
-            <div v-for="c in 12" :key="c" class="bg-gray-50 relative h-56 border text-gray-700 rounded-fd  flex items-center justify-center">
+            <div v-for="(c,i) in listChannel" :key="i" v-show="!loading" @click="detailChannel=c, $modal.show('detailChannel')"  class="bg-gray-50 relative h-56 border text-gray-700 rounded-fd  flex items-center justify-center">
                 <div class="text-center">
-                    <img src="http://localhost:3000/_nuxt/assets/svg/grabfood.svg"/>
+                    <img src="@/assets/svg/shopeefood.svg"/>
                 </div>
-                <div class="absolute bottom-0 font-bold text-sm left-0 h-12 bg-white rounded-b-fd w-full flex items-center justify-center">
-                    Grab Food
+                <div class="absolute bottom-0 text-center font-bold text-sm left-0 h-12 bg-white rounded-b-fd w-full flex items-center justify-center">
+                  <div>
+                     <span class="text-xs block">
+                     {{ c.type_name }}
+                  </span>
+                    <span class="text-sm font-bold block">
+                     {{ c.username }}
+                  </span>
+                  </div>
                 </div>
                 <div class="absolute top-0 text-xs left-0 py-3 bg-green-100 text-green-600 rounded-t-fd w-full flex items-center justify-center">
                    <div class="text-green-100 bg-green-600 h-3 w-3 rounded-full flex items-center justify-center">
@@ -38,12 +45,62 @@
 
                 </div>
             </div>
+
+
+          <div v-for="(x) in 8" :key="x" v-show="loading"  class="bg-gray-200 relative h-56 border text-gray-700 rounded-fd animate animate-pulse">
+          </div>
         </div>
 
     </div>
 <!--    modal form-->
-    <form-integrasi/>
+    <form-integrasi @reload="getList()"/>
 <!--    end modal form-->
+
+    <client-only>
+
+        <vmodal name="detailChannel" :adaptive="true" height="auto">
+          <div class="p-3">
+            <h1 class="text-center font-bold">
+              {{detailChannel.type_name}}
+            </h1>
+
+            <div>
+              <div class="text-center my-6">
+                <img src="@/assets/svg/shopeefood.svg" class="mx-auto"/>
+              </div>
+              <div class="text-center font-bold text-sm left-0 w-full flex items-center justify-center ">
+                <div>
+                  <span class="font-bold block">
+                     {{ detailChannel.username }}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-2 mt-8 mb-3">
+              <button @click.prevent="checkAcc(detailChannel.id)" :disabled="loadingCheck" class="rounded-md bg-green-500 text-white p-2">
+                <icon-base name="check"/>
+                Check
+              </button>
+              <button @click.prevent="reconnectAcc(detailChannel.id)" :disabled="loadingReconnect" class="rounded-md bg-purple-500 text-white p-2">
+                <icon-base name="repeat"/>
+                Reconnect
+              </button>
+              <button @click.prevent="syncAcc(detailChannel.id)" :disabled="loadingSync" class="rounded-md bg-blue-500 text-white p-2">
+                <icon-base name="refresh-cw"/>
+                Sync
+              </button>
+              <button @click.prevent="deleteAcc(detailChannel.id)" :disabled="loadingDelete" class="rounded-md bg-red-500 text-white p-2">
+                <icon-base name="trash-2"/>
+                Hapus
+              </button>
+            </div>
+
+
+
+          </div>
+          </vmodal>
+
+    </client-only>
   </div>
 </template>
 <script>
@@ -53,10 +110,109 @@ export default {
   components: {FormIntegrasi},
     data(){
         return {
-            listChannel: []
+          listChannel: [],
+          errorMsg: "",
+          loading: false,
+          detailChannel: {},
+          loadingDelete:false,
+          loadingCheck: false,
+          loadingReconnect: false,
+          loadingSync: false,
         }
     },
-  middleware: ["auth-ssr"]
+  middleware: ["auth-ssr"],
+  mounted() {
+    this.getList()
+    this.$modal.show('formOtp')
+  },
+  methods: {
+    async getList() {
+      try {
+        this.loading = true
+        this.errorMsg = ''
+        const res = await this.$axios.get("me/integrations");
+        this.loading = false
+        if(res.data.success) {
+          this.listChannel = res.data.data.integrations
+        } else {
+          this.errorMsg = res.data
+        }
+        console.log(res)
+      } catch (e) {
+        this.loading = false
+        console.log(e)
+      }
+    },
+    async deleteAcc(id) {
+      try {
+        this.loadingDelete = true
+        const res = await this.$axios.post("me/integrations/delete/"+id);
+        this.loadingDelete = false
+        if(res.data.success) {
+          this.$toast.success("Berhasil memutuskan koneksi")
+          this.$modal.hide('detailChannel')
+          this.getList()
+        } else {
+          this.$toast.error("Gagal memutuskan koneksi")
+        }
+        console.log(res)
+      } catch (e) {
+        this.loading = false
+        console.log(e)
+      }
+    },
+    async checkAcc(id) {
+      try {
+        this.loadingCheck = true
+        const res = await this.$axios.post("me/integrations/check/"+id);
+        this.loadingCheck = false
+        if(res.data.success) {
+          if(res.data.data.integration_success&&res.data.data.is_integrated) {
+            this.$toast.succes("Akun sudah terkoneksi")
+          } else {
+            this.$toast.error("Akun belum terkoneksi")
+          }
+        } else {
+          this.$toast.error("Gagal check koneksi")
+        }
+      } catch (e) {
+        this.loadingCheck = false
+      }
+    },
+    async syncAcc(id) {
+      try {
+        this.loadingSync = true
+        const res = await this.$axios.post("me/integrations/delete/"+id);
+        this.loadingSync = false
+        if(res.data.success) {
+          this.$toast.success("Berhasil sync koneksi")
+
+        } else {
+          this.$toast.error("Gagal sync koneksi")
+        }
+        console.log(res)
+      } catch (e) {
+        this.loadingSync = false
+        console.log(e)
+      }
+    },
+    async reconnectAcc(id) {
+      try {
+        this.loadingReconnect = true
+        const res = await this.$axios.post("me/integrations/reconnect/"+id);
+        this.loadingReconnect = false
+        if(res.data.success) {
+          this.$toast.success("Berhasil reconnect koneksi")
+        } else {
+          this.$toast.error("Gagal reconnect koneksi")
+        }
+        console.log(res)
+      } catch (e) {
+        this.loadingReconnect = false
+        console.log(e)
+      }
+    }
+  }
 
 };
 </script>
