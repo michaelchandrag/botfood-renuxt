c452b9fce16cb03c9d6dc73d87853275a50b33f0
Michael Chandra <canzinzzzide@yahoo.co.id>
2024-12-09 11:26:39 +0700

    Update index.vue

--- pages/aktifitas/index.vue
+++ pages/aktifitas/index.vue
@@ -120,7 +120,7 @@
                           <select v-model="activities[idx].reason" :id="`activity-reason-${idx}`" @change.prevent="handleUpdateItem('reason', idx)" :class="[(activity.process_reason === 1) ? 'bg-yellow-50' : ( (activity.process_reason === 2) ? 'bg-red-50' : 'bg-white' )]" class="appearance-none block w-full text-gray-700 border border-gray-300 rounded py-2 px-3 leading-tight focus:outline-none focus:border-gray-500 text-sm">
                             <option value="">Pilih Alasan</option>
                             <option v-if="activity.choices && activity.choices.length" v-for="choice in activity.choices" :value="choice.key">{{choice.text}}</option>
-                            <option value="none">Tanpa Alasan</option>
+                            <!-- <option value="none">Tanpa Alasan</option> -->
                           </select>
                           <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
